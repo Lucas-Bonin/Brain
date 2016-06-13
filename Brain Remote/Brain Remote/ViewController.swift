21dec9c560dba4bedc4c44547e5b918efb74feb4
Lucas Bonin <lucasbonin369@gmail.com>
2016-06-13 16:41:47 -0300

    Git init

---
+++ Brain Remote/Brain Remote/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Brain Remote
+//
+//  Created by Lucas Bonin on 6/13/16.
+//  Copyright © 2016 Lucas Bonin. All rights reserved.
+//
+
+import UIKit
+import VirtualGameController
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+workspace 'Brain'
+xcodeproj 'Brain Remote/Brain Remote.xcodeproj'
+xcodeproj 'Brain TV/Brain TV.xcodeproj'
+
+use_frameworks!
+
+
+target :'Brain TV' do
+	platform :tvos, '9.0'
+	xcodeproj './Brain TV/Brain TV.xcodeproj'
+	pod 'VirtualGameController', '~> 0.0'
+end
+
+
+target :'Brain Remote' do
+	platform :ios, '9.0'
+	xcodeproj './Brain Remote/Brain Remote.xcodeproj'
+	pod 'VirtualGameController', '~> 0.0'
+end	
